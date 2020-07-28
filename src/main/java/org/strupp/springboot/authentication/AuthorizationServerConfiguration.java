023fdb9dcc0228da9bfc3a58b93c486100b054a4
Michael Strupp <m.strupp@gmx.de>
2020-07-28 20:09:17 +0200

    Changed token expiration date

--- src/main/java/org/strupp/springboot/authentication/AuthorizationServerConfiguration.java
+++ src/main/java/org/strupp/springboot/authentication/AuthorizationServerConfiguration.java
@@ -43,8 +43,8 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
             .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
             .scopes("read", "write", "trust")
             .secret("secret")
-            .accessTokenValiditySeconds(120).//Access token is only valid for 2 minutes.
-            refreshTokenValiditySeconds(600);//Refresh token is only valid for 10 minutes.
+            .accessTokenValiditySeconds(86400).//Access token is only valid for 2 minutes.
+            refreshTokenValiditySeconds(10000);//Refresh token is only valid for 10 minutes.
     }
  
     @Override
