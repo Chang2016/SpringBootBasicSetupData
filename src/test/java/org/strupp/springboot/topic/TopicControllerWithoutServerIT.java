ab0eafb1db76ba03ea5793aadd7c482698068119
Michael Strupp <m.strupp@gmx.de>
2020-08-14 03:22:09 +0200

    more tests for topic

--- src/test/java/org/strupp/springboot/topic/TopicControllerWithoutServerIT.java
+++ src/test/java/org/strupp/springboot/topic/TopicControllerWithoutServerIT.java
@@ -1,13 +1,16 @@
 package org.strupp.springboot.topic;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -42,6 +45,12 @@ public class TopicControllerWithoutServerIT {
   @Value("classpath:create_topic.json")
   private Resource createJsonScript;
 
+  @Value("classpath:create_topic_with_too_short_name.json")
+  private Resource topicWithTooShortName;
+
+  @Value("classpath:update_topic.json")
+  private Resource updateJsonScript;
+
   //	Es wird gegen den tatsächlichen Wert in der DB getestet
   @Test
   public void findNonExistingTopic() throws Exception {
@@ -72,6 +81,37 @@ public class TopicControllerWithoutServerIT {
     String content = StreamUtils
         .copyToString(createJsonScript.getInputStream(), StandardCharsets.UTF_8);
     this.mockMvc.perform(post("/topics/").contentType(MediaType.APPLICATION_JSON).content(content))
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(content().string(containsString("JSON2")));
+  }
+
+  @Test
+  @Transactional
+  public void updateTopicTest() throws Exception {
+    String content = StreamUtils
+        .copyToString(updateJsonScript.getInputStream(), StandardCharsets.UTF_8);
+    this.mockMvc.perform(put("/topics/1").contentType(MediaType.APPLICATION_JSON).content(content))
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(jsonPath("$.id", is(1)))
+        .andExpect(jsonPath("$.name", is("JSON2")));
+  }
+
+  @Test
+  @Transactional
+  public void updateTopicWithTooShortNameTest() throws Exception {
+    String content = StreamUtils
+        .copyToString(topicWithTooShortName.getInputStream(), StandardCharsets.UTF_8);
+    this.mockMvc.perform(put("/topics/1").contentType(MediaType.APPLICATION_JSON).content(content))
+        .andExpect(status().is4xxClientError());
+  }
+
+  @Test
+  @Transactional
+  public void deleteTopicTest() throws Exception {
+    this.mockMvc.perform(delete("/topics/1").contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().is2xxSuccessful());
+    this.mockMvc.perform(get("/topics/1"))
+        .andDo(print())
+        .andExpect(status().isNotFound());
   }
 }
